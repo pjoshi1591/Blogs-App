6c0b1c53535988b88deb2242bb3cee6d7c72c3d6
Tejprakash <tejprakash1590@gmail.com>
2018-09-29 03:37:02 +0000

    modify database url

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ LocalStrategy    = require("passport-local"),
 expressSanitizer = require("express-sanitizer"),
 User             = require("./models/user");
 
-// mongoose.connect('mongodb://localhost:27017/restful_blog_app_v3', { useNewUrlParser: true });
-var url = process.env.DATABASEURL || "mongodb://localhost:27017/restful_blog_app";
-mongoose.connect(url, { useNewUrlParser: true });
+mongoose.connect('mongodb://pjoshi:pankaj1@ds119113.mlab.com:19113/blog', { useNewUrlParser: true });
+// var url = process.env.DATABASEURL || "mongodb://localhost:27017/restful_blog_app";
+// mongoose.connect(url, { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
